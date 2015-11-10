bc2ee5ff23bfcb571bb59712d3bb764b8214587e
swgkg <gayan@puppet.gayan.org>
2015-11-10 13:24:50 +0530

    Remove sudo from the docker build script

--- common/docker/base-image/build.sh
+++ common/docker/base-image/build.sh
@@ -41,5 +41,5 @@ popd
 echo "----------------------------------"
 echo "Building base docker image"
 echo "----------------------------------"
-sudo docker build -t wso2/base-image:${wso2_base_image_version} .
+docker build -t wso2/base-image:${wso2_base_image_version} .
 echo "Base docker image built successfully"
